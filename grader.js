2c4a4ddec4a0b3a0b4f377648d68c14c74a7b553
Ubuntu <ubuntu@ip-10-154-159-214.ec2.internal>
2013-07-18 08:47:00 +0000

    Changed port to 8080. Now should be able to preview through the AWS instance.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +36,10 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url) {
+    return url.toString();
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,6 +60,31 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile) {
+    var dummy = rest.get(url).on('complete', function(result, reponse){
+	if (result instanceof Error){
+	    console.log("%s cannot be reached.",url);
+	    process.exit(1);
+	}
+	else {
+	    var jsonChecks = checkHtml(result, checksfile);
+	    var outJson    = JSON.stringify(jsonChecks, null, 4);
+	    console.log(outJson);
+	}
+    });
+}
+
+var checkHtml = function(html, checksfile) {
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for (var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+}
+
 var clone = function(fn) {
     /* Workaround for commander.js issue. */
     /* http://stackoverflow.com/a/6772648 */
@@ -65,12 +95,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of index.html', clone(assertUrlExists), null)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url == null) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson   = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else {
+	checkUrl(program.url, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
-
